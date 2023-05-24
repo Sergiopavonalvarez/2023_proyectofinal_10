39796b6bfa257bb6061cef1d42d242f20ae5a3cd
mpavon17 <spavon17@gmail.com>
2023-05-25 01:44:09 +0200

    java scrip de peliculon

--- JS/js_peliculon.js
+++ JS/js_peliculon.js
@@ -1,1 +1,20 @@
+function cambiarFondo(selector, imagen, event, titulo, descripcion) {
+    event.preventDefault();
+    var elemento = document.querySelector(selector);
+    elemento.style.backgroundImage = 'url("' + imagen + '")';
+    elemento.style.backgroundSize = 'cover';
+
+    // Cambiar los botones de reproducir e información
+    var reproducirBtn = document.querySelector(selector + ' .reproducir');
+    var informacionBtn = document.querySelector(selector + ' .informacion');
+    reproducirBtn.href = 'https://www.youtube.com/watch?v=_F5-E1_J1yo';
+    informacionBtn.href = 'https://www.filmaffinity.com/es/filmNuevaURL';
+
+    // Cambiar el texto y el título
+    var tituloElemento = document.querySelector(selector + ' .titulo');
+    var descripcionElemento = document.querySelector(selector + ' .descripcion');
+    tituloElemento.textContent = titulo;
+    descripcionElemento.textContent = descripcion;
+  }
+
 
