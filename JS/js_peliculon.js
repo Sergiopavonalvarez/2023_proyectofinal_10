2efd0030e1f7ef4dbdfe12827fd9c29a10cac7ba
mpavon17 <spavon17@gmail.com>
2023-05-25 14:25:42 +0200

    colores de titulos y descripcion

--- JS/js_peliculon.js
+++ JS/js_peliculon.js
@@ -1,26 +1,30 @@
 
 
-  function cambiarFondo(selector, imagen, event, titulo, descripcion, enlaceReproducir, enlaceInformacion) {
-    event.preventDefault();
-    var elemento = document.querySelector(selector);
-    elemento.style.backgroundImage = 'url("' + imagen + '")';
-    elemento.style.backgroundSize = 'cover';
-  
-    // Cambiar los botones de reproducir e información
-    var reproducirBtn = document.querySelector(selector + ' .reproducir');
-    var informacionBtn = document.querySelector(selector + ' .informacion');
-    reproducirBtn.href = enlaceReproducir;
-    informacionBtn.href = enlaceInformacion;
-  
-    // Cambiar el texto y el título
-    var tituloElemento = document.querySelector(selector + ' .titulo');
-    var descripcionElemento = document.querySelector(selector + ' .descripcion');
-    tituloElemento.textContent = titulo;
-    descripcionElemento.textContent = descripcion;
-    var tituloElemento = document.querySelector(selector + ' .titulo');
-var descripcionElemento = document.querySelector(selector + ' .descripcion');
-var enlaceReproducir = '';
-var enlaceInformacion = '';
+function cambiarFondo(selector, imagen,color, event, titulo, descripcion, enlaceReproducir, enlaceInformacion) {
+  event.preventDefault();
+  var elemento = document.querySelector(selector);
+  elemento.style.backgroundImage = 'url("' + imagen + '")';
+  elemento.style.backgroundSize = 'cover';
+
+  // Cambiar los botones de reproducir e información
+  var reproducirBtn = document.querySelector(selector + ' .reproducir');
+  var informacionBtn = document.querySelector(selector + ' .informacion');
+  reproducirBtn.href = enlaceReproducir;
+  informacionBtn.href = enlaceInformacion;
+
+  // Cambiar el texto y el título
+  var tituloElemento = document.querySelector(selector + ' .titulo');
+  var descripcionElemento = document.querySelector(selector + ' .descripcion');
+  tituloElemento.textContent = titulo;
+  descripcionElemento.textContent = descripcion;
+  var tituloElemento = document.querySelector(selector + ' .titulo');
+  var descripcionElemento = document.querySelector(selector + ' .descripcion');
+  var enlaceReproducir = '';
+  var enlaceInformacion = '';
+
+  tituloElemento.style.color = color;
+  descripcionElemento.style.color = color;
+
 }
 
 
