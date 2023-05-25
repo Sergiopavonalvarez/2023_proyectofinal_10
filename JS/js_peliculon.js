84d8e161aae8ae893c13e2f7e4ad34278885c018
mpavon17 <spavon17@gmail.com>
2023-05-25 11:28:42 +0200

    todo modificado

--- JS/js_peliculon.js
+++ JS/js_peliculon.js
@@ -1,20 +1,27 @@
-function cambiarFondo(selector, imagen, event, titulo, descripcion) {
+
+
+  function cambiarFondo(selector, imagen, event, titulo, descripcion, enlaceReproducir, enlaceInformacion) {
     event.preventDefault();
     var elemento = document.querySelector(selector);
     elemento.style.backgroundImage = 'url("' + imagen + '")';
     elemento.style.backgroundSize = 'cover';
-
+  
     // Cambiar los botones de reproducir e información
     var reproducirBtn = document.querySelector(selector + ' .reproducir');
     var informacionBtn = document.querySelector(selector + ' .informacion');
-    reproducirBtn.href = 'https://www.youtube.com/watch?v=_F5-E1_J1yo';
-    informacionBtn.href = 'https://www.filmaffinity.com/es/filmNuevaURL';
-
+    reproducirBtn.href = enlaceReproducir;
+    informacionBtn.href = enlaceInformacion;
+  
     // Cambiar el texto y el título
     var tituloElemento = document.querySelector(selector + ' .titulo');
     var descripcionElemento = document.querySelector(selector + ' .descripcion');
     tituloElemento.textContent = titulo;
     descripcionElemento.textContent = descripcion;
   }
+ var enlaceReproducir = '';
+var enlaceInformacion = '';
+
+cambiarFondo('#selector', 'ruta/imagen.jpg', event, 'Nuevo título', 'Nueva descripción', enlaceReproducir, enlaceInformacion);
+
 
 
