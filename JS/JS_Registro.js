5e22e39250ed1070525ccf7be16932e256b636ed
mpavon17 <spavon17@gmail.com>
2023-06-03 13:32:42 +0200

    js de registro de personas

--- JS/JS_Registro.js
+++ JS/JS_Registro.js
@@ -1,25 +1,41 @@
 function registrar() {
-    var nombre = document.getElementById("registro_nombre").value;
-    var contrasena = document.getElementById("registro_contrasena").value;
+  var nombre = document.getElementById("username").value;
+  var contrasena = document.getElementById("password").value;
 
-    var usuario = {
-      nombre: nombre,
-      contrasena: contrasena
-    };
+  var usuario = {
+    nombre: nombre,
+    contrasena: contrasena
+  };
 
-    // Verificar si hay registros almacenados en localStorage
-    var registros = localStorage.getItem("registros");
-    if (registros) {
-      registros = JSON.parse(registros);
-    } else {
-      registros = [];
-    }
-
-    // Agregar el nuevo usuario al arreglo de registros
-    registros.push(usuario);
-
-    // Almacenar los registros actualizados en localStorage
-    localStorage.setItem("registros", JSON.stringify(registros));
-
-    alert("Registro exitoso. Ahora puedes iniciar sesión.");
+  // Verificar si hay registros almacenados en localStorage
+  var registros = localStorage.getItem("registros");
+  if (registros) {
+    registros = JSON.parse(registros);
+  } else {
+    registros = [];
   }
+
+  // Agregar el nuevo usuario al arreglo de registros
+  registros.push(usuario);
+
+  // Almacenar los registros actualizados en localStorage
+  localStorage.setItem("registros", JSON.stringify(registros));
+
+  alert("Registro exitoso. Ahora puedes iniciar sesión.");
+  var checkboxTerms = document.getElementById("terms");
+var btnRegistrarse = document.querySelector('input[type="submit"]');
+
+// Deshabilitar el botón de "Registrarse" inicialmente
+btnRegistrarse.disabled = true;
+
+// Agregar evento de cambio al checkbox
+checkboxTerms.addEventListener("change", function() {
+  // Habilitar o deshabilitar el botón de "Registrarse" según el estado del checkbox
+  btnRegistrarse.disabled = !checkboxTerms.checked;
+});
+window.location.href = "login.html";
+}
+
+
+
+  
